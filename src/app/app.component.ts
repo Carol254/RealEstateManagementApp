c8ab050c8743823adadc3a6739606c8dc8cf36d0
Caroline <wanguic49@gmail.com>
2023-06-14 12:27:32 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  standalone:true,
+  imports: [HomeComponent],
+  template: `<main>
+                <header class="brand-name">
+                  <i class="bi bi-house-heart-fill"></i>
+                </header>
+
+                <section class="content">
+                  <app-home></app-home>
+                </section>
+            </main>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
