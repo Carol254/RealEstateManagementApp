39b41d482ffa43405af1d4f69fe30aa2e4df3ce4
Caroline <wanguic49@gmail.com>
2023-06-17 10:32:54 +0300

    few changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './home/home.component';
   imports: [HomeComponent],
   template: `<main>
                 <header class="brand-name">
-               <img src="/assets/house-fill.svg">
+                <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
                 </header>
 
                 <section class="content">
