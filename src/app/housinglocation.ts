de717912f4a30de3e270fc1419102938d53d0b4c
Caroline <wanguic49@gmail.com>
2023-06-19 16:39:16 +0300

    added properties to the interface

--- src/app/housinglocation.ts
+++ src/app/housinglocation.ts
@@ -1,2 +1,11 @@
 export interface Housinglocation {
+    id:number;
+    name:string;
+    city:string;
+    state:string;
+    photo:string;
+    availableUnits:number;
+    wifi:boolean;
+    laundry:boolean;
+
 }
