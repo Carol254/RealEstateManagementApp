db6817a23f439f904f89a47ae1828d502b594729
Caroline <wanguic49@gmail.com>
2023-06-21 17:27:33 +0300

    few changes made

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -7,10 +7,10 @@ import { Housinglocation } from '../housinglocation';
   standalone: true,
   imports: [CommonModule],
   template: `
-     <section class="listing">
-    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    <section class="listing">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
   </section>
   `,
   styleUrls: ['./housing-location.component.css']
