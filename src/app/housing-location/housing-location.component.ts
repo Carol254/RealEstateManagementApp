315bc5f90eb8e4f7e8f91c712ee2d6cfe666973c
Caroline <wanguic49@gmail.com>
2023-06-23 22:51:05 +0300

    added changes with items id

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,20 @@
 import { Component,Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Housinglocation } from '../housinglocation';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+            RouterLink,
+            RouterOutlet],
   template: `
     <section class="listing">
       <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
   </section>
   `,
   styleUrls: ['./housing-location.component.css']
