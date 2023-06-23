082c3f7aa6b02bd46f2b6393aa35465a64d75887
Caroline <wanguic49@gmail.com>
2023-06-23 23:12:04 +0300

    few changes made

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      details works!
-    </p>
+    <p>details works! {{ housingLocationId }}</p>
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+
+  route:ActivatedRoute = inject(ActivatedRoute);
+
+  housingLocationId = -1 ;
+
+  constructor(){
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 
 }
