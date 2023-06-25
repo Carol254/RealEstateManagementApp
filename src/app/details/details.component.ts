a82210324f58955e7d4ce73d03f9a658daa52113
Caroline <wanguic49@gmail.com>
2023-06-25 22:18:46 +0300

    few changes made

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -20,8 +20,8 @@ import { Housinglocation } from '../housinglocation';
       <h2 class="section-heading">About this housing location</h2>
       <ul>
         <li>Units available: {{housingLocation?.availableUnits}}</li>
-        <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
-        <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
+        <li>Does this location have wifi?{{housingLocation?.wifi}}</li>
+        <li>Does this location have laundry?{{housingLocation?.laundry}}</li>
       </ul>
     </section>
   </article>
