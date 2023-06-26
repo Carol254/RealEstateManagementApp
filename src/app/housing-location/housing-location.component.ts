50145bdc2b8fe2c74e43a5f9efbb6f7240215b6b
Caroline <wanguic49@gmail.com>
2023-06-26 22:23:43 +0300

    few changes made

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -14,7 +14,8 @@ import { RouterLink, RouterOutlet } from '@angular/router';
       <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
-      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+      <button [routerLink]="['/details', housingLocation.id]" class="details-link">Learn More</button>
+      
   </section>
   `,
   styleUrls: ['./housing-location.component.css']
