588db5ea6c1676ead4ae20f79dcd1258d57baab4
Caroline <wanguic49@gmail.com>
2023-06-27 22:45:59 +0300

    sending formdata to the service

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -106,4 +106,9 @@ export class HousingService {
   getHousingLocationById(id:number):Housinglocation | undefined{
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
   }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
+  }
+  
 }
