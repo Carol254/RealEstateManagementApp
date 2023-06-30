bd79d91f371f4f57429c42cab3ad4719f9921c9d
Caroline <wanguic49@gmail.com>
2023-06-30 10:29:45 +0300

    added filtering in the housing list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
+import { Housinglocation } from './../housinglocation';
 import { Component,inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-import { Housinglocation } from '../housinglocation';
 import { HousingService } from '../housing.service';
 
 @Component({
@@ -12,14 +12,14 @@ import { HousingService } from '../housing.service';
   template: `
      <section>
     <form>
-      <input type="text" placeholder="Filter by city...">
-      <button class="primary" type="button">Search Here</button>
+      <input type="text" placeholder="Filter by city..." #filter>
+      <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
     </form>
   </section>
 
   <section class="results">
     <app-housing-location 
-    *ngFor="let housingLocation of housingLocationList"
+    *ngFor="let housingLocation of filteredLocationList"
     [housingLocation]="housingLocation"> </app-housing-location>
   </section>
    
@@ -35,4 +35,13 @@ export class HomeComponent {
     this.housingLocationList = this.housingService.getAllHousingLocations();
     this.filteredLocationList = this.housingLocationList;
   }
+
+  filterResults(text:string){
+    if(!text){
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(housinglocation => housinglocation?.city.toLowerCase().includes(text.toLowerCase()));
+  }
+  
 }
